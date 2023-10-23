f2269c929932ef3284177c63eba6eb71c99aba01
Asmakhan29 <asmakhan2942@gmail.com>
2023-10-23 13:26:13 +0530

    js

--- keyword.js
+++ keyword.js
@@ -1,7 +1,12 @@
 n = 34;
 if(n%2 == 0){
     console.log("even");
+    myvar = 543;
+    let a = 'nice';
+    const b = 4.12;
 }else{
     console.log("odd");
 }
-console.log("hello");
+console.log(myvar);
+// console.log(a);
+console.log(b);
