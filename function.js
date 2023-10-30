d1684a996b86ed3aa3bb16277e6bd485aa4ce486
Asmakhan29 <asmakhan2942@gmail.com>
2023-10-30 13:38:27 +0530

    js

--- function.js
+++ function.js
@@ -16,3 +16,30 @@ const getavg = function(m1, m2, m3){
 // console.log(getavg(47,50,65));
 const ans = getavg(47,50,65);
 console.log(ans);
+
+
+//using arrow function
+ const factorial = (n) => {
+    let fact = 1;
+    for(let i =2; i<=n ; i++){
+        fact *= i;
+    }
+    return fact;
+ };
+
+ console.log(factorial(5));
+
+ const newfunction = factorial;
+ console.log(newfunction(5));
+
+
+ // callback function
+ const permutation = (n, r, fact_function) => {
+    let ans = fact_function(n)/fact_function(n-r);
+    console.log("permutation isn", ans);
+ }
+
+ permutation(10, 3, factorial);
+
+
+ 
