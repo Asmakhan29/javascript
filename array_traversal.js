5a46f56b41c2236098db13f1e3f688e0990eb3b0
Asmakhan29 <asmakhan2942@gmail.com>
2023-10-31 13:25:11 +0530

    js

--- array_traversal.js
+++ array_traversal.js
@@ -15,5 +15,21 @@ nums.forEach((n) => {            //better to apply for time complexity
 
 //create a new array that contains square of all elts
 
+//map
 const newarr = nums.map((n) => { return n**2;})
 console.log(newarr);
+
+//filter
+const even = nums.filter( (a) => { return a%2 === 0} );
+console.log(even);
+
+const price = [2300, 150, 1300, 800, 300, 600, 650];
+const filprice = price.filter( (n) => { return (n>=500 && n <= 1000)})
+console.log(filprice);
+
+const brands = ['abc@yahoo.com','mmm@gmail.com','xyz@gmail.com','jjj@hotmail.com'];
+
+console.log("jsdjn@gmail.com".includes('mail'));
+
+const newmail = brands.filter((a) => { return a.includes('gmail')});
+console.log(newmail);
